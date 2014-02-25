2334390922afdcdf65ea518eb5fa7e82c8f883e9
Leo Ying <zying@andrew.cmu.edu>
2014-02-24 19:48:25 -0500

    done

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,2 +1,17 @@
 class Child < ActiveRecord::Base
+
+#relationship 
+has_many :chores
+has_many :tasks, through: :chores
+
+#validation
+validates_presence_of :first_name, :last_name
+
+#scopes
+scope :alphabetical, ->{order('last_name,first_name')}
+
+scope :active, ->{where(active:true)}
+def name
+  first_name + " " + last_name
+end
 end
