fa9e20b9316896c851e7de316b3ac83fd972d89a
Leo Ying <zying@andrew.cmu.edu>
2014-02-24 20:10:15 -0500

    all task tests pass

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,12 @@
 class Chore < ActiveRecord::Base
+#relationship
+has_many :chores
+has_many :children, through: :chores
+
+#validation
+validates_presence_of :name
+validates_numericality_of :ponints
+
+#scopes
+scope :alphabetical, ->{order ()}
 end
